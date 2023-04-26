9e04d912284cef32aeb48b3be91a1fdfe33df4ad
apselon <iladam@icloud.com>
2023-04-26 19:25:28 +0300

    HW2done

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 istioctl install --set profile=minimal -y
 
 kubectl label namespace default istio-injection=enabled
-kubectl apply -f K8s/pokemon-deployment.yml
-
-kubectl apply -f K8s/ingress-gateway.yml
-kubectl apply -f K8s/ingress-service.yml
-
-kubectl apply -f K8s/egress-gateway.yml
-kubectl apply -f K8s/egress-service.yml
 
 kubectl apply -f K8s/nginx-deployment.yml
+kubectl apply -f K8s/pokemon-deployment.yml
+
+kubectl apply -f K8s/ingress-service.yml
+kubectl apply -f K8s/ingress-gateway.yml
+
+kubectl apply -f K8s/egress-service.yml
+kubectl apply -f K8s/egress-gateway.yml
 
 minikube tunnel
